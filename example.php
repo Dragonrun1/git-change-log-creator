6635e09095dccf511d33c06e0cd3870294caa369
Dragonrun1 <Dragonrun1@gmail.com>
2019-02-10 20:00:16 -0800

    Updated example.php license and code for PHP 7.1+

--- example.php
+++ example.php
@@ -1,13 +1,15 @@
 <?php
+declare(strict_types=1);
+
 /**
- * Contains GitChangeLogCreator class.
+ * Contains php example using GitChangeLogCreator class.
  *
- * PHP version 5.4
+ * PHP version 7.1
  *
  * LICENSE:
  * This file is part of git-change-log-creator which is used to create an
  * updated change log file from Git log.
- * Copyright (C) 2014 Michael Cummings
+ * Copyright (C) 2014-2019 Michael Cummings
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
@@ -20,14 +22,15 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
+ * <https://opensource.org/licenses/GPL-2.0>.
  *
  * You should be able to find a copy of this license in the LICENSE file.
  *
- * @copyright 2014 Michael Cummings
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU GPL
+ * @copyright 2014-2019 Michael Cummings
+ * @license   GPL-2.0
  * @author    Michael Cummings <mgcummings@yahoo.com>
  */
+
 use GitChangeLogCreator\GitChangeLogCreator;
 
 include_once __DIR__ . '/bootstrap.php';
